8d5db17e0f67e4581994d9dc6b8c5b6a2736a77c
Md. Sharear Saon <sharear.saon@slu.edu>
2025-07-08 10:45:37 -0400

    added lines to download structurs

--- scripts/script_to_download.py
+++ scripts/script_to_download.py
@@ -1,5 +1,26 @@
 import os
 import pandas as pd
+from subprocess import call
 
 
+#import list of structures to be downloaded
+df_str= pd.read_csv()
+home= os.getcwd()
+str_files=[]
+str_dir= '' #directory where we want to save the structures
+os.chdir(str_dir)
 
+for i, j in enumerate(df_str['PDB_ID']):
+    str_files.append(j)
+
+#downloading the structures
+for filename in str_files:
+    full_path= os.path.join(str_dir, filename+'.cif')
+    if os.path.isfile(full_path):
+        print ('FILE ALREADY EXISTS! NOT DOWNLOADING!')
+    else: 
+        print ('Downloading '+ filename)
+        url = "http://www.rcsb.org/pdb/files/%s.cif" %filename
+        call (["curl","-L","-O","-s",url])
+
+os.chdir(home)
